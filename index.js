2ebbea9e1519fb117665fcd9998200eaba96eb28
phamhieu1222 <hieu712202@gmail.com>
2025-03-13 16:15:15 +0700

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function tinhtoan(){
     let a = document.getElementById("input").value;
+    const muc1 = 1806;
+    const muc2 = 1866;
+    const muc3 = 2167;
+    const muc4 = 2729;
     let result;
     if(a<50){
-        result = a*1806
+        result = a*muc1
     }
     else if(a<100){
-        result = 50*1806+(a-50)*1866;
+        result = 50*muc1+(a-50)*muc2;
     }
     else if(a<200){
-        result = 50*1806+100*1866+(a-100)*2167;
+        result = 50*muc1+100*muc2+(a-100)*muc3;
     }
     else{
-        result = 50*1806+100*1866+200*2167+(a-200)*2729;
+        result = 50*muc1+100*muc2+200*muc3+(a-200)*muc4;
     }
 document.getElementById("tien").innerHTML = result;
 
